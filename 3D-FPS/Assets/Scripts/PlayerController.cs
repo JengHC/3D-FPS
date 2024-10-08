d88abc2fdff7aa0783e88c72dd03267c730a6915
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-10-08 15:52:44 +0900

    3D FPS

--- 3D-FPS/Assets/Scripts/PlayerController.cs
+++ 3D-FPS/Assets/Scripts/PlayerController.cs
@@ -3,9 +3,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float WalkingSpeed = 7;
+    public float walkingSpeed = 7;
+    public float mouseSens = 0.01f;
+    public Transform CameraTransform;
+
+    float horizontalAngle;
+    float verticalAngle;
 
     InputAction moveAction;
+    InputAction lookAction;
 
     CharacterController characterController;
 
@@ -15,11 +21,16 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        InputActionAsset inputActions = GetComponent<PlayerInput>().actions;
+        InputActionAsset inputActions = GetComponent<PlayerInput>().actions; // PlayerInput에 actions를 가지고 와서 
 
-        moveAction = inputActions.FindAction("Move");
+        moveAction = inputActions.FindAction("Move");                        // move를 사용
+        lookAction = inputActions.FindAction("Look");
 
         characterController = GetComponent<CharacterController>();
+
+        verticalAngle = 0;
+        horizontalAngle = transform.localEulerAngles.y;
+
     }
 
     // Update is called once per frame
@@ -34,9 +45,27 @@ public class PlayerController : MonoBehaviour
             move.Normalize();
         }
 
-        move = move * WalkingSpeed * Time.deltaTime;
+        move = move * walkingSpeed * Time.deltaTime;
         move = transform.TransformDirection(move);  // 현재 gameObject의 방향으로 벡터를 돌림
         characterController.Move(move);             // character controller componenet 움직여
 
+        Vector2 look = lookAction.ReadValue<Vector2>();     // look액션 벡터를 가져옴
+
+        float turnPlayer = look.x * mouseSens;              // 마우스 감도 적용
+        horizontalAngle += turnPlayer;                      //현재 각도에 더함
+        if (horizontalAngle >= 360) horizontalAngle -= 360;
+        if (horizontalAngle <0) horizontalAngle += 360;
+        
+        Vector3 currentAngle = transform.localEulerAngles;  // 변한된 현재 각도를 넣는다.
+        currentAngle.y = horizontalAngle;
+        transform.localEulerAngles =currentAngle;
+
+        // 마우스 상하 
+        float turnCam = look.y * mouseSens;
+        verticalAngle -= turnCam;
+        verticalAngle = Mathf.Clamp(verticalAngle, -89f,89f);
+        currentAngle = CameraTransform.localEulerAngles;
+        currentAngle.x = verticalAngle;
+        CameraTransform.localEulerAngles = currentAngle;
     }
 }
